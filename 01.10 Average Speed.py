16f791dce7d92987bee5b459f0476a1546cfe09d
deewee54 <decrosswhite@ualr.edu>
2025-02-05 15:17:58 +0000

    Update

--- 01.10 Average Speed.py
+++ 01.10 Average Speed.py
@@ -3,6 +3,7 @@ minutes = int(input("Enter Minutes: "))
 seconds = int(input("Enter Seconds: "))
 miles  = length / 1.61
 seconds = seconds / 60
-hour = seconds + minutes / 60
+minutes = minutes + seconds
+hour = minutes / 60 
 mph = miles / hour
 print(mph)

4a6d326154c1aeca57fb1207c4e5a95c2cbb793e
deewee54 <decrosswhite@ualr.edu>
2025-02-10 16:04:15 +0000

    update

--- 02.09 Fractional Part.py
+++ 02.09 Fractional Part.py
@@ -1,3 +1,4 @@
 number = float(input("Enter a Number: "))
 number = number % 1
+number = number // 1
 print("Fractional Part:",number)
---
+++ 02.Project.py
@@ -0,0 +1,7 @@
+import math 
+print("Great Circle Calculator")
+radius = float(input("Enter Radius of Sphere: "))
+StartingLA = float(input("Starting Point - Enter Latitude: "))
+StartingLO = float(input("Starting Point - Enter Longitude: "))
+EndingLA = float(input("Ending Point - Enter Latitude: "))
+EndingLO = float(input("Ending Point - Enter Longitude: "))
